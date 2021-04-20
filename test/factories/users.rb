dc6ac2dbbed45f52258f05b462516b4b87399f27
paul-reed-smith <forall@mm.st>
2021-04-20 14:51:57 +0300

    add factories tests

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,8 +3,20 @@ FactoryBot.define do
     first_name
     last_name
     password
-    email { 'MyString' }
-    avatar { 'MyString' }
+    email
+    avatar
     type { '' }
+
+    factory :developer do
+      type { 'Developer' }
+    end
+
+    factory :manager do
+      type { 'Manager' }
+    end
+
+    factory :admin do
+      type { 'Admin' }
+    end
   end
 end
