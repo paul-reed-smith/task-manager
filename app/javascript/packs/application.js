feb6254385a5d002f4a83d52a352ace4b679b356
Dmitry <paulreed@fastmail.com>
2021-05-11 21:32:16 +0300

    Feature/kanban board (#16)
    * makefile fix
    
    * add kanban-board

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,11 @@
 
 import Rails from '@rails/ujs';
 import * as ActiveStorage from '@rails/activestorage';
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from '../components/TaskBoard';
 import 'material-design-lite/material.js';
 import 'channels';
 
 Rails.start();
 ActiveStorage.start();
+WebpackerReact.setup({ TaskBoard });
