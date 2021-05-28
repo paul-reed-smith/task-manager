677b67d88286f8e8d0b1e6db1cda11c3d260966d
paul-reed-smith <forall@mm.st>
2021-05-28 13:32:17 +0300

    edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,27 @@ admin.password = 'admin'
 admin.save
 
 60.times do |i|
-  u = [Manager, Developer].sample.new
-  u.email = "email#{i}@mail.gen"
-  u.first_name = "FN#{i}"
-  u.last_name = "LN#{i}"
-  u.password = "#{i}"
+  u = [Developer, Manager].sample.new
+  if u.type == 'Developer'
+    u.email = "dev#{i}@mail.gen"
+    u.first_name = "developer#{i}"
+    u.last_name = "ldeveloper#{i}"
+  else
+    u.email = "man#{i}@mail.gen"
+    u.first_name = "manager#{i}"
+    u.last_name = "lmanager#{i}"
+  end
+  u.password = i.to_s
   u.save
+end
+
+states = ['new_task', 'in_development', 'in_qa', 'in_code_review', 'ready_for_release', 'released', 'archived']
+
+60.times do |i|
+  t = Task.new
+  t.name = "name#{i}"
+  t.description = "description#{i}"
+  t.author_id = i
+  t.state = states.sample
+  t.save
 end
